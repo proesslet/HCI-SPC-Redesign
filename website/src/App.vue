34d0fca2e33bbae95c9955297fd4185290317a51
Preston Roesslet <proesslet@gmail.com>
2024-03-11 12:24:09 -0500

    set up base styles and navbar

--- website/src/App.vue
+++ website/src/App.vue
@@ -5,6 +5,8 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <header>
     <div class="wrapper">
+      <!-- Todo: Add NOAA and SPC logos -->
+      <h1>Storm Prediction Center</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
       </nav>
@@ -14,4 +16,30 @@ import { RouterLink, RouterView } from "vue-router";
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Navbar styles */
+header {
+  background-color: var(--dark-blue);
+  padding: 2rem 2rem;
+}
+
+header h1 {
+  color: var(--white);
+  font-size: 2rem;
+  margin: 0;
+}
+
+header nav a {
+  color: var(--white);
+  text-decoration: none;
+}
+
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 1rem;
+}
+</style>
